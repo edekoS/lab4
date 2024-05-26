2183b5bf7f2c778e41c11f85d32657a0650ca539
edekoS <makarovdddddd@gmail.com>
2024-05-27 00:35:44 +0200

    Add --help option

--- skrypt.sh
+++ skrypt.sh
@@ -1,2 +1,3 @@
 date
 for i in {1..100}; do echo "log$i" > log$i.txt; echo "log$i.txt - skrypt.sh - $(date)"; done
+if [ "$1" == "--help" ]; then echo "--date: show current date"; echo "--logs: create 100 log files"; echo "--logs <number>: create <number> log files"; fi
